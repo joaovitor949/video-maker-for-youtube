54ed4f0cef4f0fac6a940602003c634171a3a392
joaovitor949 <pagamentosjoao7@gmail.com>
2021-06-06 18:16:16 -0300

    feat:

--- robots/image.js
+++ robots/image.js
@@ -1,10 +1,9 @@
 const imageDownloader = require('image-downloader')
-const gm = require('gm').subClass({imageMagick: true})
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const state = require('./state.js')
 
-const googleSearchCredentials = require('../credentials/google-search.json')
+const googleSearchCredentials = require('./credentials/google-search.json').apiKey .searchEngineId
 
 async function robot() {
   console.log('> [image-robot] Starting...')
@@ -12,9 +11,6 @@ async function robot() {
 
   await fetchImagesOfAllSentences(content)
   await downloadAllImages(content)
-  await convertAllImages(content)
-  await createAllSentenceImage(content)
-  await createYouTubeThumbnail(content)
 
   state.save(content)
 
@@ -34,11 +30,11 @@ async function robot() {
       num: 2
     })
 
-    const imagesUrl = response.data.items.map((item) => {
-      return item.link
-    })
+    // const imageUrl = response.data.items[fetchGoogleAndReturnImagesLinks].map((item) => {
+    //   return item.link
+    // })
 
-    return imagesUrl
+    // return imageUrl
   }
 
   async function downloadAllImages(content) {
@@ -70,126 +66,6 @@ async function robot() {
     return imageDownloader.image({
       url: url,
       dest: `./content/${fileName}`
-    })
-  }
-  async function convertAllImages(content) {
-    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
-      await convertImage(sentenceIndex)
-      
-    }
-
-    async function convertImage(sentenceIndex) {
-      return new Promise((resolve, reject) => {
-        const inputFile = `./content/${sentenceIndex}-original.png[0]`
-        const outputFile = `.content/${sentenceIndex}-coverted.png`
-        const width = 1920
-        const height = 1080
-
-        gm()
-        .in(inputFile)
-        .out('(')
-          .out('-clone')
-          .out('0')
-          .out('-background', 'white')
-          .out('-blur', '0x9')
-          .out('-resize', `${width}x${height}`)
-        .out(')')
-        .out('(')
-          .out('-clone')
-          .out('0')
-          .out('-background', 'white')
-          .out('-resize', `${width}x${height}`)
-        .out(')')
-        .out('-delete', '0')
-        .out('-gravity', 'center')
-        .out('-compose', 'over')
-        .out('-composite')
-        .out('-extent', `${width}x${height}`)
-        .write(outputFile, (error) => {
-          if (error) {
-            return reject(error)
-          }
-
-          console.log(`> Image converted : ${inputFile}`)
-          resolve()
-        })
-      })
-    }
-  }
-
-  async function createAllSentenceImage(content) {
-    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
-      await createSentenceImage(sentenceIndex, content.sentences[sentenceIndex].text)
-      
-    }
-
-  }
-
-  async function createSentenceImage(sentenceIndex, sentenceText) {
-    return new Promise((resolve, reject) => {
-      const outputFile = `./content/${sentenceIndex}-sentence.png`
-
-      const templeteSettings = {
-        0: {
-          size: '1920x400',
-          gravity: 'center'
-        },
-        1: {
-          size: '1920x1080',
-          gravity: 'center'
-        },
-        2: {
-          size: '800x1080',
-          gravity: 'center'
-        },
-        3: {
-          size: '1920x400',
-          gravity: 'center'
-        },
-        4: {
-          size: '1920x1080',
-          gravity: 'center'
-        },
-        5: {
-          size: '800x1080',
-          gravity: 'center'
-        },
-        6: {
-          size: '1920x400',
-          gravity: 'center'
-        },
-
-      }
-
-      gm()
-      .out('-size', templeteSettings[sentenceIndex].size)
-      .out('-gravity', templeteSettings[sentenceIndex].gravity)
-      .out('-background', 'tranparent')
-      .out('-fill', 'white')
-      .out('-kerning', '-1')
-      .out(`caption:${sentenceText}`)
-      .write(outputFile, (error) => {
-        if (error) {
-          return reject(error)
-        }
-        console.log(`> Sentence created: ${outputFile}`)
-        resolve()
-      })
-    })
-  }
-
-  async function createYouTubeThumbnail() {
-    return new Promise((resolve, reject) => {
-      gm()
-        .in('./content/0-converted.png')
-        .write('./content/youtube-thumbnail.jpg', (error) => {
-          if (error) {
-            return reject(error)
-          }
-
-          console.log('> Creating YouTube Thumbnail')
-          resolve()
-        })
     })
   }
 
